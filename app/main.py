c55d3fdbbdbd21a24d8f334713d1d0830465be57
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-07-21 17:48:43 -0500

    Register the Route of post method created at api in main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-
+from app.api import api
 app = FastAPI()
 
 @app.get('/')
 def root():
     return {'message':'Welcome to PeerUp, a skill_swap API'}
+
+app.include_router(api.router)
 
     
