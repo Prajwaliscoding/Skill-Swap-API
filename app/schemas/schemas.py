166c411113c2feeb3d5aeefebe5de04f35381df7
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-07-24 12:06:35 -0500

    Added some comments in schemas.py

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,31 +1,33 @@
 # schemas.py
 from pydantic import BaseModel, EmailStr
 
-class CreateUser(BaseModel):
+class CreateUser(BaseModel):     # for validating the data entered to store user in database.
     name: str
     email: EmailStr
     password: str
 
 
-class UserDataOut(BaseModel):
+class UserDataOut(BaseModel):    # for validating the data given to the user as response
     id: int
     name: str
     email: EmailStr
     is_active: bool
 
     model_config = {
-        "from_attributes": True
+        "from_attributes": True    
     }
-    
 
 
-# 📦 Context: You're using Pydantic v2
-# In Pydantic v1, we used:
 
-# class Config:
-#     orm_mode = True
-# In Pydantic v2, the configuration style changed. Now you use:
+# 📦 Context: We're using Pydantic v2
+# In Pydantic v2, the configuration style changed. Now we use:
 
 # model_config = {
 #     "from_attributes": True
 # }
+
+
+# The data is stored in the database.
+# When you query the database, you get SQLAlchemy objects.
+# But FastAPI can't return those objects as JSON directly, so you use a Pydantic model with
+# model_config = {"from_attributes": True} to convert those SQLAlchemy objects into JSON for the response.
