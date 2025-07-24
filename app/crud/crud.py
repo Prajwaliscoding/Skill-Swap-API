e7acd96ced0fd3dcb3a358fbc34993687212879c
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-07-24 12:41:01 -0500

    Removed comments from crud.py

--- app/crud/crud.py
+++ app/crud/crud.py
@@ -1,20 +1,19 @@
 # crud.py
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session       
 from app.models.models import Users
 from app.schemas.schemas import CreateUser
-from passlib.context import CryptContext  # type: ignore
+from passlib.context import CryptContext  # type: ignore   
 
-# need to install first with pip install "passlib[bcrypt]"
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") 
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password)    
 
-def create_user(db:Session,  user:CreateUser):
+def create_user(db:Session,  user:CreateUser):     
     hashed_pw = get_password_hash(user.password)
-    db_user = Users(name=user.name, email=user.email, hashed_password=hashed_pw)
+    db_user = Users(name=user.name, email=user.email, hashed_password=hashed_pw) 
     db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+    db.commit()     
+    db.refresh(db_user)  
     return db_user
