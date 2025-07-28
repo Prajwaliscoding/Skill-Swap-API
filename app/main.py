f7ed579446925df93c4da896457d504cc57e979c
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-07-28 11:27:51 -0500

    Added router for /login to main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 # main.py
 from fastapi import FastAPI
-from app.api import user
+from app.api import user, auth
+
 app = FastAPI()
 
 @app.get('/')
 def root():
     return {'message':'Welcome to PeerUp, a skill_swap API'}
 
-app.include_router(user.router)    
+app.include_router(user.router) 
+
+app.include_router(auth.router)  
