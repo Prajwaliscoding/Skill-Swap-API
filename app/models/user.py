259a2a096c87100af9cd4da085b3aca6558025b6
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-08-02 13:38:27 -0500

    Added Skills Model and established relationship with Users

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 # models/user.py
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from app.db.database import Base
 
 class Users(Base):
@@ -9,3 +10,13 @@ class Users(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default = True)
+    skill = relationship("Skills", back_populates="owner", cascade="all, delete")
+
+class Skills(Base):
+    __tablename__ = 'skills'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable = False)
+    skill_type = Column(String, nullable=False)
+    user_id = Column (Integer, ForeignKey = "users.id")
+    owner = relationship("Users", back_populates= "skill")
+
