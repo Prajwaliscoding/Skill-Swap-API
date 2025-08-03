f3cacdb255aee4ce7387a469535a0e50dcaad5f2
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-08-03 14:54:22 -0500

    Added delete skill endpoint

--- app/api/skill.py
+++ app/api/skill.py
@@ -1,10 +1,10 @@
 # app/api/skill.py
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas.skill import SkillCreate, SkillOut
 from app.db.database import get_db
 from app.models.user import Skills, Users
-from app.crud.skill import create_skill, get_user_skills, get_all_skills
+from app.crud.skill import create_skill, delete_skill, get_user_skills, get_all_skills, delete_skill
 from app.dependencies.auth import get_current_user
 from typing import List
 
@@ -17,8 +17,22 @@ def add_skill(skill_data: SkillCreate, db: Session =  Depends(get_db), current_u
 
 @router.get("/", response_model= List[SkillOut])
 def list_user_skills(db: Session = Depends(get_db), current_user: Users = Depends(get_current_user)):
-    return get_user_skills(db, current_user)
+    skill = get_user_skills(db, current_user)
+    if not skill:
+        raise HTTPException(status_code=404, detail="Skill not Found")
+    return skill
 
 @router.get("/all", response_model= List[SkillOut])
 def list_all_skills(db: Session = Depends(get_db), current_user: Users = Depends(get_current_user)):
     return get_all_skills(db)
+
+@router.delete("/delete/{skill_id}")
+def remove_skill(skill_id: int, db: Session = Depends(get_db), current_user: Users = Depends (get_current_user)):
+    skill = delete_skill(skill_id, db, current_user)
+
+    if not skill:
+        raise HTTPException(status_code=404, detail="Skill not Found")
+
+    db.delete(skill)
+    db.commit()
+    return {"message": "Skill deleted."}
--- app/crud/skill.py
+++ app/crud/skill.py
@@ -7,7 +7,7 @@ def create_skill(skill_data: SkillCreate, db: Session,current_user: Users):
     db_skill = Skills(
         name = skill_data.name, 
         skill_type = skill_data.skill_type,
-        user_id = current_user.id )      #  Link skill to the logged-in user
+        user_id = current_user.id )      
     
     db.add(db_skill)
     db.commit()
@@ -19,3 +19,7 @@ def get_user_skills(db: Session, current_user: Users):
 
 def get_all_skills(db: Session):
     return db.query(Skills).all()
+
+def delete_skill(skill_id:int, db:Session, current_user: Users):
+    return db.query(Skills).filter(Skills.id == skill_id, Skills.user_id == current_user.id).first()   # Match by skill ID, Ensure the skill belongs to logged-in user
+
